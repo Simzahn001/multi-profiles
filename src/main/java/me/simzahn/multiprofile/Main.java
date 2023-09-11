307886406790edd64b79ad18305b0f510fce9cb3
Simzahn001 <hochenberger.simon@ortweinschule.at>
2023-09-11 21:01:49 +0200

    Initial commit

---
+++ src/main/java/me/simzahn/multiprofile/Main.java
@@ -0,0 +1,17 @@
+package me.simzahn.multiprofile;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MultiProfile extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
